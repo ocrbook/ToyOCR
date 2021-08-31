95e3bfe939c17e69e80fba33f93dfbeae8442ac4
刘树春 <shuchun.lsc@alibaba-inc.com>
2021-08-31 21:26:28 +0800

    Add eval

---
+++ eval_toydet.sh
@@ -0,0 +1,1 @@
+python train_net.py --config-file yamls/text_detection/toydet_text.yaml --num-gpus 1 --eval-only
--- modeling/detectors/toydet.py
+++ modeling/detectors/toydet.py
@@ -77,25 +77,25 @@ class ToyDet(nn.Module):
         if not self.training:
             # return self.inference(images)
             rets = self.inference(images, batched_inputs)
+            if DEBUG:
+                for batch, result in zip(copy_imgs, rets):
+                    img = batch["image"]
 
-            for batch, result in zip(copy_imgs, rets):
-                img = batch["image"]
+                    img = img.cpu().detach().numpy().transpose((1, 2, 0))
 
-                img = img.cpu().detach().numpy().transpose((1, 2, 0))
+                    img = img[:, :, ::-1]
+                    img = img.astype(np.int8)
 
-                img = img[:, :, ::-1]
-                img = img.astype(np.int8)
+                    # img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+                    instances = result["instances"]
+                    rboxes = instances.rboxes
 
-                # img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-                instances = result["instances"]
-                rboxes = instances.rboxes
-
-                for rbox in rboxes:
-                    rbox = np.array(rbox, np.int)
-                    img = cv2.polylines(
-                        img, [rbox], True, color=(0, 0, 155), thickness=2)
-                # cv2.imshow("hi",img)
-                # cv2.waitKey(0)
+                    for rbox in rboxes:
+                        rbox = np.array(rbox, np.int)
+                        img = cv2.polylines(
+                            img, [rbox], True, color=(0, 0, 155), thickness=2)
+                    # cv2.imshow("hi",img)
+                    # cv2.waitKey(0)
             return rets
 
         image_shape = images.tensor.shape[-2:]
@@ -105,33 +105,35 @@ class ToyDet(nn.Module):
         gt_segm = ImageList.from_tensors(
             gt_segm, 32, 0).tensor
 
-        segm_show = (gt_segm[0].detach().cpu().numpy()*255).astype(np.uint8)
-        x, y = segm_show.shape[0:2]
-        segm_show = cv2.resize(segm_show, (int(y/4), int(x/4)))
-        cv2.imshow("gt_segm", segm_show)
+        if DEBUG:
+            segm_show = (gt_segm[0].detach().cpu().numpy()
+                         * 255).astype(np.uint8)
+            x, y = segm_show.shape[0:2]
+            segm_show = cv2.resize(segm_show, (int(y/4), int(x/4)))
+            cv2.imshow("gt_segm", segm_show)
 
         mask = [x["mask"].to(self.device) for x in batched_inputs]
 
         mask = ImageList.from_tensors(
             mask, 32, 0).tensor
+        if DEBUG:
+            mask_show = (mask[0].detach().cpu().numpy()*255).astype(np.uint8)
 
-        img_mask = (mask[0].detach().cpu().numpy()*255).astype(np.uint8)
-
-        x, y = img_mask.shape[0:2]
-        img_mask = cv2.resize(img_mask, (int(y/4), int(x/4)))
-        cv2.imshow("mask", img_mask)
+            x, y = mask_show.shape[0:2]
+            mask_show = cv2.resize(mask_show, (int(y/4), int(x/4)))
+            cv2.imshow("mask", mask_show)
 
         features = self.backbone(images.tensor)
 
         up_fmap = self.upsample(features)
         preds = self.head(up_fmap)
 
-        img_show = preds[0].detach().cpu().numpy()[0]
+        if DEBUG:
+            img_show = preds[0].detach().cpu().numpy()[0]
 
-        cv2.imshow("det", (img_show*255).astype(np.uint8))
-        cv2.waitKey(2)
+            cv2.imshow("det", (img_show*255).astype(np.uint8))
+            cv2.waitKey(2)
 
-        # loss=mse_loss(preds,gt_segm)
 
         loss_segm = self.head.losses(preds, gt_segm, mask)
 
